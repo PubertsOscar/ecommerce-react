6e817fb084824d740f6d3248d1db98dc9736df2b
PubertsOscar <esquivelkazuli@gmail.com>
2023-02-28 15:35:21 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, { ...book, quantity: 1 }]);
+  }
+
+  function changeQuantity(book, quantity) {
+    setCart(
+      cart.map((item) =>
+        item.id === book.id
+          ? {
+              ...item,
+              quantity: +quantity,
+            }
+          : item
+      )
+    );
+  }
+
+  function removeItem(item) {
+    setCart(cart.filter(book=>book.id !== item.id))
+  }
+
+  function numberOfItems(){
+    let counter = 0
+    cart.forEach(item => {
+        counter += item.quantity
+    });
+    return counter
+  }
+  
+  useEffect(() => {}, [cart]);
+
+  return (
+    <Router>
+      <Nav numberOfItems={numberOfItems()}/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books books={books} cart={cart} />} />
+        <Route
+          path="/books/:id"
+          element={<BookInfo books={books} addToCart={addToCart} cart={cart} />}
+        />
+        <Route
+          path="/cart"
+          element={
+            <Cart
+              books={books}
+              cart={cart}
+              changeQuantity={changeQuantity}
+              removeItem={removeItem}
+            />
+          }
+        />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
